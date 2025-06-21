451e74af99da7a8dbea2cd091800b47a74d5ad35
rakshith_s <rakshithsantosh@gmail.com>
2025-06-21 18:20:31 +0530

    created a user schema and established a connection with DB

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,35 @@
 const express = require("express");
 
 const app = express(); //creating a new express js server
+const connectDB = require("./config/database");
 
-app.listen(3000);
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const userObj = {
+    firstName: "joe",
+    lastName: "doe",
+    emailId: "joedoe@gmail.com",
+    password: "joeDoe@1234",
+  };
+  //creating a new instance of the user model
+  const user = new User(userObj);
+
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (e) {
+    console.error("error saving user", e);
+  }
+});
+
+connectDB()
+  .then(() => {
+    console.log("DB is connected");
+    app.listen(3000, () => {
+      console.log("server is listening on 3000");
+    });
+  })
+  .catch((e) => {
+    console.error("database is not connected", e);
+  });
