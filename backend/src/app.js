c113c8521a08551773ea43ea00a692cf9398021c
rakshith_s <rakshithsantosh@gmail.com>
2025-06-25 09:53:24 +0530

    data sanitization and schema validation

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,21 +5,55 @@ const connectDB = require("./config/database");
 
 const User = require("./models/user");
 
+app.use(express.json()); //this is used to parse incoming json data(request body) to be specific the json data attached in the request is parsed into javascript object
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "joe",
-    lastName: "doe",
-    emailId: "joedoe@gmail.com",
-    password: "joeDoe@1234",
-  };
+  // const userObj = {
+  //   firstName: "joe",
+  //   lastName: "doe",
+  //   emailId: "joedoe@gmail.com",
+  //   password: "joeDoe@1234",
+  // };
   //creating a new instance of the user model
-  const user = new User(userObj);
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("user added successfully");
   } catch (e) {
     console.error("error saving user", e);
+  }
+});
+
+//get all users /user
+app.get("/feed", async (req, res) => {
+  try {
+    await User.find({}).then((users) => {
+      res.send(users);
+    });
+  } catch (error) {
+    console.error("error getting users", e);
+  }
+});
+
+//get specific users /feed
+app.get("/user", async (req, res) => {
+  try {
+    await User.find({ emailId: req.body.emailId }).then((user) => {
+      res.send(user);
+    });
+  } catch (error) {
+    console.error("error getting users", e);
+  }
+});
+
+//delete user /user
+app.delete("/user", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body._id);
+    res.send("user deleted successfully");
+  } catch (error) {
+    console.error("error getting users", e);
   }
 });
 
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,24 +1,70 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("password is weak");
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      //validate function only works when creating a new document not when updating
+      validate(value) {
+        if (value !== "male" && value !== "female" && value !== "other") {
+          throw new Error("Invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid url");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "I am a software developer",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
